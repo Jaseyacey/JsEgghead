c4229b5df57d8c06ec7587ad1dc1078000180e29
Jaseyacey <41111290+Jaseyacey@users.noreply.github.com>
2023-05-12 11:33:01 +0100

    getTotalPrice and thunk and cart actions

--- redux-shopping-cart/src/features/cart/cartSlice.test.ts
+++ redux-shopping-cart/src/features/cart/cartSlice.test.ts
@@ -1,4 +1,21 @@
-import cartReducer, { addToCart, CartState, removeFromCart, updateQuantity } from './cartSlice'
+import cartReducer, { getTotalPrice, addToCart, CartState, removeFromCart, updateQuantity, getMemoizedNumItems, checkoutCart } from './cartSlice'
+import type { RootState } from '../../app/store';
+import products from '../../../public/products.json';
+import * as api from '../../app/api';
+
+jest.mock('../../app/api', () => {
+    return {
+        async getProducts() {
+            return []
+        },
+        async checkout(items: api.CartItems = {}) {
+            const empty = Object.keys(items).length === 0;
+            if (empty) throw new Error("Must include cart items");
+            if (items.badItem > 0) return { success: false };
+            return { success: true }
+        }
+    }
+})
 
 describe('cart reducer', () => {
     test('an empty action', () => {
@@ -49,4 +66,182 @@ describe('cart reducer', () => {
             items: { abc: 1, def: 5 },
         });
     });
+    describe("selectors", () => {
+        describe("getMemoizedNumItems", () => {
+            it("should return 0 with no items", () => {
+                const cart: CartState = {
+                    checkoutState: "READY",
+                    errorMessage: "",
+                    items: {},
+                };
+                const result = getMemoizedNumItems({ cart } as RootState);
+                expect(result).toEqual(0);
+            });
+            it("should add up the total", () => {
+                const cart: CartState = {
+                    checkoutState: "READY",
+                    errorMessage: "",
+                    items: { abc: 3, def: 3 },
+                };
+                const result = getMemoizedNumItems({ cart } as RootState);
+                expect(result).toEqual(6);
+            });
+            it("should not compute again with the same state", () => {
+                const cart: CartState = {
+                    checkoutState: "READY",
+                    errorMessage: "",
+                    items: { abc: 3, def: 3 },
+                };
+                getMemoizedNumItems.resetRecomputations();
+                getMemoizedNumItems({ cart } as RootState);
+                expect(getMemoizedNumItems.recomputations()).toEqual(1);
+                getMemoizedNumItems({ cart } as RootState);
+                expect(getMemoizedNumItems.recomputations()).toEqual(1);
+                getMemoizedNumItems({ cart } as RootState);
+                getMemoizedNumItems({ cart } as RootState);
+                getMemoizedNumItems({ cart } as RootState);
+                expect(getMemoizedNumItems.recomputations()).toEqual(1);
+            });
+            it("should recompute with new state", () => {
+                const cart: CartState = {
+                    checkoutState: "READY",
+                    errorMessage: "",
+                    items: { abc: 3, def: 3 },
+                };
+                getMemoizedNumItems.resetRecomputations();
+                getMemoizedNumItems({ cart } as RootState);
+                expect(getMemoizedNumItems.recomputations()).toEqual(1);
+                cart.items = { abc: 2 };
+                getMemoizedNumItems({ cart } as RootState);
+                expect(getMemoizedNumItems.recomputations()).toEqual(2);
+            });
+        });
+    });
+    describe('getTotalPrice', () => {
+        it('should return 0 with an empty cart', () => {
+            const state: RootState = {
+                cart: { checkoutState: "READY", errorMessage: '', items: {} },
+                products: { products: {} }
+            }
+            const result = getTotalPrice(state)
+            expect(result).toEqual('0.00')
+        });
+        it('should add up the totals', () => {
+            const state: RootState = {
+                cart: {
+                    checkoutState: "READY", errorMessage: '', items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    }
+                },
+                products: {
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1],
+                    }
+                }
+            }
+            const result = getTotalPrice(state);
+            expect(result).toEqual('43.23');
+        });
+        it('should not compute again with the same state', () => {
+            const state: RootState = {
+                cart: {
+                    checkoutState: "READY", errorMessage: '', items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    }
+                },
+                products: {
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1],
+                    }
+                }
+            }
+
+            getTotalPrice.resetRecomputations()
+            const result = getTotalPrice(state);
+            expect(result).toEqual('43.23');
+            expect(getTotalPrice.recomputations()).toEqual(1)
+            getTotalPrice(state)
+            expect(getTotalPrice.recomputations()).toEqual(1)
+        })
+        it("should recompute with new products", () => {
+            const state: RootState = {
+                cart: {
+                    checkoutState: "READY",
+                    errorMessage: "",
+                    items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    },
+                },
+                products: {
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1],
+                    },
+                },
+            };
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            state.products.products = {
+                [products[0].id]: products[0],
+                [products[1].id]: products[1],
+                [products[2].id]: products[2],
+            };
+            result = getTotalPrice({ ...state });
+            expect(result).toEqual("43.23");
+            expect(getTotalPrice.recomputations()).toEqual(2);
+        });
+
+        it("should recompute when cart changes", () => {
+            const state: RootState = {
+                cart: {
+                    checkoutState: "READY",
+                    errorMessage: "",
+                    items: {
+                        [products[0].id]: 3,
+                        [products[1].id]: 4,
+                    },
+                },
+                products: {
+                    products: {
+                        [products[0].id]: products[0],
+                        [products[1].id]: products[1],
+                    },
+                },
+            };
+            getTotalPrice.resetRecomputations();
+            let result = getTotalPrice(state);
+            expect(result).toEqual("43.23");
+            expect(getTotalPrice.recomputations()).toEqual(1);
+            state.cart.items = {};
+            result = getTotalPrice({ ...state });
+            expect(result).toEqual("0.00");
+            expect(getTotalPrice.recomputations()).toEqual(2);
+        });
+    });
 });
+
+describe('thunks', () => {
+    describe('checkoutCart w/mocked dispatch', () => {
+        it('should checkout', async () => {
+            const dispatch = jest.fn();
+            const state: RootState = {
+                products: { products: {} },
+                cart: { checkoutState: 'READY', errorMessage: '', items: { abc: 1, } }
+            }
+            const thunk = checkoutCart();
+            await thunk(dispatch, () => state, undefined);
+            console.log('asdasdasdasdasd', dispatch.mock.calls)
+            const { calls } = dispatch.mock;
+            expect(calls).toHaveLength(2)
+            console.log(calls[0], calls[2])
+        })
+        it.todo('should fail with no items')
+    })
+})
